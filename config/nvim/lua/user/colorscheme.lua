ff5307bf420230dfd6298cebbb1b9aaf52e53edb
Jacob Morris <jacob.morris1410@gmail.com>
2022-03-06 20:07:07 -0500

    Change colorscheme

--- config/nvim/lua/user/colorscheme.lua
+++ config/nvim/lua/user/colorscheme.lua
@@ -2,7 +2,7 @@
 vim.cmd "colorscheme default"
 
 -- local colorscheme = "spacecamp"
-local colorscheme = "moonfly"
+local colorscheme = "gruvbox"
 
 -- Try to set colorscheme -- if it doesn't work then keep as default
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
