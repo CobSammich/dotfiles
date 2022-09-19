2c59d234a5d6883c381a899f6b8d398395d6b45d
Jacob Morris <jacob.morris1410@gmail.com>
2022-09-19 18:12:21 -0400

    Change fold highlighting

--- config/nvim/lua/user/custom_colors.lua
+++ config/nvim/lua/user/custom_colors.lua
@@ -4,6 +4,9 @@ vim.cmd[[hi SpellBad cterm=underline gui=undercurl ctermfg=red guisp=red guifg=r
 vim.cmd[[hi clear SpellCap]]
 vim.cmd[[hi SpellCap cterm=underline gui=undercurl ctermfg=lightred guisp=lightred guifg=orange]]
 
+vim.cmd[[highlight clear Folded]]
+vim.cmd[[highlight Folded guibg=none ctermbg=none guifg=lightgreen gui=italic]]
+
 -- Honestly not sure what these are.. but they highlight actual words
 -- vim.cmd[[hi clear SpellLocal]]
 -- vim.cmd[[hi clear SpellRare]]
