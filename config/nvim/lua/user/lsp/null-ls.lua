fd5c9c99035a035b42d79560cbb287152e82d3b7
Jacob Morris <jacob.morris1410@gmail.com>
2022-11-08 22:40:49 -0500

    prioritize python autocomplete and remove spellcheck autocomplete -- null_ls

--- config/nvim/lua/user/lsp/null-ls.lua
+++ config/nvim/lua/user/lsp/null-ls.lua
@@ -48,11 +48,11 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 require("null-ls").setup({
     sources = {
-        require("null-ls").builtins.formatting.stylua,
-        require("null-ls").builtins.diagnostics.eslint,
-        require("null-ls").builtins.completion.spell,
         require("null-ls").builtins.formatting.autopep8,
         require("null-ls").builtins.diagnostics.pylint,
+        require("null-ls").builtins.formatting.stylua,
+        require("null-ls").builtins.diagnostics.eslint,
+        --require("null-ls").builtins.completion.spell,
     },
     on_attach = function(client, bufnr)
         if client.supports_method("textDocument/formatting") then
