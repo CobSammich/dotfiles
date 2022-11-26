d177814be5cb319f07eff945ca68f3fdd7c34f0e
Jacob Morris <jacob.morris1410@gmail.com>
2022-11-26 08:18:11 -0500

    Script for toggling off monitors

---
+++ config/scripts/toggle_multiple_monitors.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Toggles display between single monitor and multiple monitors
+
+if [[ $1 == 1 ]]; then
+    # single monitor
+    xrandr --output DP-2 --primary --output HDMI-0 --off --output HDMI-1 --off
+    feh --no-fehbg --bg-scale '/home/cob/Pictures/wallpapers/space_station.png'
+elif [[ $1 == 2 ]]; then
+    # 2 monitors
+    xrandr --output HDMI-0 --mode 1920x1080 --rate 60 --rotate left --auto --pos 0x0 --output DP-2 --pos 1080x240 --primary
+    feh --no-fehbg --bg-scale '/home/cob/Pictures/wallpapers/dmaztb4adsp61.png' '/home/cob/Pictures/wallpapers/vertical/purple_gradient.jpg'
+elif [[ $1 == 3 ]]; then
+    # 3 monitors
+    xrandr --output HDMI-0 --mode 1920x1080 --rate 60 --rotate left --auto --pos 0x0 --output HDMI-1 --auto --pos 3640x600 --output DP-2 --pos 1080x240 --primary
+    feh --no-fehbg --bg-scale '/home/cob/Pictures/wallpapers/dmaztb4adsp61.png' '/home/cob/Pictures/wallpapers/vertical/purple_gradient.jpg' '/home/cob/Pictures/wallpapers/everblush_ign_astronaut.png'
+else
+    echo "Currently only support for 3 monitors. \"$1\" is not valid. Choose 1, 2, or 3"
+fi
--- home/.bash_aliases
+++ home/.bash_aliases
@@ -38,6 +38,7 @@ alias openrgb="sudo /home/cob/Applications/OpenRGB_0.7_x86_64_6128731.AppImage"
 alias show_network_devices="watch -n 5 $HOME/.config/scripts/show_network_devices.sh"
 alias latex_clean="$HOME/.config/scripts/latex_clean.sh"
 alias publish_site="$HOME/.config/scripts/publish_site.sh"
+alias toggle_monitors="$HOME/.config/scripts/toggle_multiple_monitors.sh"
 # Run the nba-scores-cli program I developed
 alias nba="~/Coding/projects/nba-scores-cli/target/release/nba-scores-cli"
 
