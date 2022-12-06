c03b0edd36bdd3e2e1169f047c5f36c3a8d0cdc0
Jacob Morris <jacob.morris1410@gmail.com>
2022-12-05 20:06:14 -0500

    Skeletons for python files

--- config/nvim/lua/user/core/autocommands.lua
+++ config/nvim/lua/user/core/autocommands.lua
@@ -10,8 +10,15 @@ vim.api.nvim_command([[
 ]])
 
 -- Vimiwki related
-vim.cmd [[au FileType vimwiki set syntax=pandoc]]
+vim.cmd([[au FileType vimwiki set syntax=pandoc]])
 -- vim.cmd [[au FileType vimwiki set filetype=markdown]]
 
 -- vim.cmd [[set foldtext=getline(v:foldstart+1)]]
-vim.cmd [[autocmd FileType python setlocal foldenable foldmethod=syntax]]
+vim.cmd([[autocmd FileType python setlocal foldenable foldmethod=syntax]])
+
+-- skeletons
+vim.api.nvim_create_autocmd("BufNewFile", {
+    pattern = "*.py",
+    group = vim.api.nvim_create_augroup("create_skeletons", { clear = true }),
+    command = "0r ~/.config/nvim/templates/skeleton.py"
+})
