3f202779703087a5539c0c9ac2bbc2e229edb80c
Jacob Morris <jacob.morris1410@gmail.com>
2023-07-30 16:07:52 -0400

    Moving neovim config to be based on NvChad

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,24 +1,21 @@
-require "core.options"
-require "core.keymaps"
-require "core.autocommands"
+require "core"
 
-require "plugins.packer"
-require "plugins.bufferline"
-require "plugins.nvim-tree"
-require "plugins.lualine"
-require "plugins.indent_blankline"
-require "plugins.gitsigns"
-require "plugins.telescope"
-require "plugins.treesitter"
-require "plugins.cmp"
-require "plugins.project"
-require "plugins.aerial"
-require "plugins.vim-doge"
-require "plugins.autopairs"
-require "plugins.colorizer"
+local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
 
-require "lsp.lsp"
+if custom_init_path then
+  dofile(custom_init_path)
+end
 
--- Single line setups:
-require('Comment').setup()
-require('fidget').setup()
+require("core.utils").load_mappings()
+
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+-- bootstrap lazy.nvim!
+if not vim.loop.fs_stat(lazypath) then
+  require("core.bootstrap").gen_chadrc_template()
+  require("core.bootstrap").lazy(lazypath)
+end
+
+dofile(vim.g.base46_cache .. "defaults")
+vim.opt.rtp:prepend(lazypath)
+require "plugins"
--- config/nvim/lua/plugins/vim-doge.lua
+++
@@ -1,1 +0,0 @@
-vim.cmd [[let g:doge_doc_standard_python = "numpy"]]
