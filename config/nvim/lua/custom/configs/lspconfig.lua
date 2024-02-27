4f99099e2d68f375b882c4afe51e8aea83b8b4c3
Jacob Morris <jacob.morris1410@gmail.com>
2024-02-26 20:24:47 -0500

    Add formatting for markdown files using marksman

--- config/nvim/lua/custom/configs/lspconfig.lua
+++ config/nvim/lua/custom/configs/lspconfig.lua
@@ -1,19 +1,27 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "clangd", "pyright", "rust_analyzer", "texlab", "dockerls" }
+local servers = {
+	"html",
+	"clangd",
+	"pyright",
+	"rust_analyzer",
+	"texlab",
+	"dockerls",
+	"marksman",
+}
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
-  vim.diagnostic.config({
-    virtual_text = false,
-  })
+	lspconfig[lsp].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
+	vim.diagnostic.config({
+		virtual_text = false,
+	})
 end
 
 --
